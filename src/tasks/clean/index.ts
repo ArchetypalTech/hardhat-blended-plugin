016e7cd3b7a7847a4b80b169be8043879fd45c79
d1r1 <me@d1r1.me>
2024-11-28 18:38:42 +0400

    feat(clean:rust): add task to remove rust build artifacts

---
+++ src/tasks/clean/index.ts
@@ -0,0 +1,27 @@
+import { task } from 'hardhat/config';
+import { TASK_CLEAN } from 'hardhat/builtin-tasks/task-names';
+import { RustCompiler } from '../../internal/compiler';
+import { Logger, LogLevel } from '../../utils/logger';
+
+const TASK_CLEAN_RUST = 'clean:rust';
+
+task(TASK_CLEAN_RUST, 'Cleans Rust artifacts').setAction(async (_, hre) => {
+  const compiler = new RustCompiler(hre.config.fluent, hre);
+
+  try {
+    await compiler.clean();
+  } catch (error) {
+    throw error;
+  }
+});
+
+task(TASK_CLEAN, 'Cleans the cache and deletes all artifacts').setAction(
+  async (_, hre, runSuper) => {
+    try {
+      await runSuper();
+      await hre.run(TASK_CLEAN_RUST);
+    } catch (error) {
+      throw error;
+    }
+  },
+);
